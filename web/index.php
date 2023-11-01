63fe304e12ca1726cf7b61a094b051b8a25e2f2d
pc <pc@ABDULLAH-PC>
2023-11-02 02:47:32 +0500

    settings

--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
 <?php
-echo 'MI care APIS';
-phpinfo();
+switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+    case '/':
+        require 'index.php';
+        break;
+    case '/login':
+        require 'services/login.php';
+        break;
+    default:
+        http_response_code(404);
+        exit('Not Found');
+}
